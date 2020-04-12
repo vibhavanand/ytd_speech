d22914e97e06619e873a686db4990df48020f11a
Vibhav Anand <vibhav.anand@exponentiadata.com>
2020-04-13 02:43:08 +0530

    resolving characs error

--- project_file.py
+++ project_file.py
@@ -10,8 +10,7 @@ import string
 import pandas as pd
 
 def ytd_audio_splitting_enhacing_mapping(youtube_link,download_location,split_location,transformation_location,noise_file_location):
-
-    random = ''.join([random.choice(string.ascii_letters + string.digits) for n in range(5)])
+    characs = ''.join([random.choice(string.ascii_letters + string.digits) for n in range(5)])
     youtube_file_name="banking"+characs+".wav"
     # youtube_file_name="bankingbZytQ.wav"
     youtube_dir=download_location
