ec4ade273d233ee56f14abe5a996049a64f8f907
Vibhav Anand <vibhav.anand@exponentiadata.com>
2020-04-13 02:45:29 +0530

    fixing error

--- project_file.py
+++ project_file.py
@@ -11,6 +11,7 @@ import pandas as pd
 
 def ytd_audio_splitting_enhacing_mapping(youtube_link,download_location,split_location,transformation_location,noise_file_location):
     characs = ''.join([random.choice(string.ascii_letters + string.digits) for n in range(5)])
+    print(characs)
     youtube_file_name="banking"+characs+".wav"
     # youtube_file_name="bankingbZytQ.wav"
     youtube_dir=download_location
